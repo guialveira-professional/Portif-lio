1a7b5e62d2aecc96af70cc65eda2815b094c9cee
guialveira-professional <guilhermeaoliveira.profissional@gmail.com>
2024-08-02 10:01:25 -0300

    comments

--- utils/chatbot_personal.py
+++ utils/chatbot_personal.py
@@ -4,27 +4,27 @@ from llama_index.llms.ollama import Ollama
 from llama_index.core import SimpleDirectoryReader
 from llama_index.readers.file import PDFReader
 
-#def chatbot():
+def chatbot():
     
-    #parser = PDFReader()
-    #file_extractor = {".pdf": parser}
-    #documents = SimpleDirectoryReader(
-    #"docs", file_extractor=file_extractor
-    #).load_data()
+    parser = PDFReader()
+    file_extractor = {".pdf": parser}
+    documents = SimpleDirectoryReader(
+    "docs", file_extractor=file_extractor
+    ).load_data()
     
-    #ollama_embedding = OllamaEmbedding(
-    #    model_name="llama3",
-    #    base_url="http://localhost:11434", 
-    #    ollama_additional_kwargs={"mirostat": 0},
-    #)
-    #Settings.embed_model = ollama_embedding
-#
- #   Settings.llm = Ollama(model="llama3", temperature=0.2)
-#
- #   index = VectorStoreIndex.from_documents(
-  #      documents,
-   # )
+    ollama_embedding = OllamaEmbedding(
+        model_name="llama3",
+        base_url="http://localhost:11434", 
+        ollama_additional_kwargs={"mirostat": 0},
+    )
+    Settings.embed_model = ollama_embedding
 
-    #query_engine = index.as_query_engine()
+    Settings.llm = Ollama(model="llama3", temperature=0.2)
 
-    #return query_engine
+    index = VectorStoreIndex.from_documents(
+        documents,
+    )
+
+    query_engine = index.as_query_engine()
+
+    return query_engine
